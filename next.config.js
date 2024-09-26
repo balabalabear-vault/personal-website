3ba4e8b5c5b2a2d098d672a9f98c65cfd16e8187
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-09-26 12:21:51 -0400

    fix: include data.json in the build file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ dns.setDefaultResultOrder("ipv4first");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'standalone',
   images: {
     remotePatterns: [
       {
