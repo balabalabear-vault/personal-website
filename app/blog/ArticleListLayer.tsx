81477d9e5b5fba8165cf96ba57cac2d783db9c13
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-09-26 11:56:22 -0400

    fix: forcing dynamic fetch

--- app/blog/ArticleListLayer.tsx
+++ app/blog/ArticleListLayer.tsx
@@ -27,3 +27,4 @@ export default async function ArticleListLayer({
         </Box>
     )
 }
+export const dynamic = 'force-dynamic';
--- app/project/ProjectListLayer.tsx
+++ app/project/ProjectListLayer.tsx
@@ -20,3 +20,4 @@ export default async function ProjectListLayer({
         </Box>
     )
 }
+export const dynamic = 'force-dynamic';
