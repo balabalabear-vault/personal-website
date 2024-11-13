5a070c3005bdb8f040c8278f5acdb4700942b6d2
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-11-13 12:52:03 -0500

    fix: removed unnecessary layer

--- app/blogs/cardLayout/Lists.tsx
+++ app/blogs/cardLayout/Lists.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { Paper, styled, Typography } from "@mui/material";
+import { Paper, Stack, styled, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import { DateTime } from "luxon";
+import { useRouter } from "next/navigation";
+import { TBlog } from "../../api/posts/route";
 import CategoryLayer from "../../components/CategoryLayer/CategoryLayer";
 import JBox from "../../components/JBox/JBox";
-import JLink from "../../components/JLink/JLink";
-import { DateTime } from "luxon";
-import { TArticleListLayer } from "../ArticleListLayer";
+import JEmptyList from "../../components/JEmptyList/JEmptyList";
 
 const StyledTypography = styled(Typography)({
     display: '-webkit-box',
@@ -16,43 +17,56 @@ const StyledTypography = styled(Typography)({
     marginTop: '8px'
 });
 
+type TArticleListLayer = {
+    blogs: TBlog[] | undefined
+}
+
 export default function Lists({
     blogs
 }: Readonly<TArticleListLayer>) {
+    const router = useRouter();
+    const handleClick = (url: string) => router.push(url);
     return (
-        <>
+        <Grid container spacing={2} mb={1} alignItems="stretch">
             {
-                blogs.map(({
-                    id,
-                    categories,
-                    title,
-                    content,
-                    createdAt,
-                }) => (
-                    <Grid size={{ xs: 12, sm: 6 }} key={id}>
-                        <JLink href={`/blogs/${id}`} newTab={false} noDecoration>
-                            <Paper sx={{
-                                padding: 2,
-                                '&:hover': {
-                                    cursor: 'pointer',
-                                    elevation: 3
-                                }
-                            }}>
-                                <CategoryLayer categories={categories} clickable={false} />
-                                <JBox needsDivider>
-                                    <Typography variant="h6">{title}</Typography>
-                                </JBox>
-                                <StyledTypography variant="body1">
-                                    {content}
-                                </StyledTypography>
-                                <Typography variant="subtitle2" textAlign="right">
-                                    {DateTime.fromISO(createdAt).setLocale('en').toLocaleString(DateTime.DATE_FULL)}
-                                </Typography>
-                            </Paper>
-                        </JLink>
-                    </Grid>
-                ))
+                blogs && blogs.length
+                    ? (
+                        blogs.map(({
+                            id,
+                            categories,
+                            title,
+                            content,
+                            createdAt,
+                        }) => (
+                            <Grid
+                                key={id}
+                                size={{ xs: 12, sm: 6 }}
+                                onClick={() => handleClick(`/blogs/${id}`)}
+                            >
+                                <Paper sx={{
+                                    padding: 2,
+                                    '&:hover': {cursor: 'pointer' },
+                                    height: "100%"
+                                }}>
+                                    <Stack height="100%">
+                                        <CategoryLayer categories={categories} clickable={false} />
+                                        <JBox needsDivider>
+                                            <Typography variant="h6">{title}</Typography>
+                                        </JBox>
+                                        <StyledTypography variant="body1">
+                                            {content}
+                                        </StyledTypography>
+                                        <Typography variant="subtitle2" textAlign="right" marginTop="auto">
+                                            {DateTime.fromISO(createdAt.toString()).setLocale('en').toLocaleString(DateTime.DATE_FULL)}
+                                        </Typography>
+                                    </Stack>
+                                </Paper>
+                            </Grid>
+                        ))
+                    ) : (
+                        <JEmptyList />
+                    )
             }
-        </>
+        </Grid>
     )
 }
