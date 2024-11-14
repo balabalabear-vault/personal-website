0426712b61156d6dcef760307e9d67ec731267b4
Kwok Yui Kuen <88313566+kuenyuikwok1106@users.noreply.github.com>
2024-11-14 14:03:53 -0500

    feat: creating a layout tsx for the page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,23 +1,13 @@
-import { Box } from "@mui/material";
-import Header from "../components/Header/Header";
 import Content from "./Content";
-import { Metadata } from "next/types";
-
-export const metadata: Metadata = {
-    title: "Blogs",
-};
 
 export default async function Page() {
     const data = (await fetch('http://127.0.0.1:3000/api/categories?type=blog'));
     const { data: categories } = await data.json();
     return (
-        <Box>
-            <Header title="Blog" subtitle="My instagram." />
-            <Content
-                categories={categories}
-                clickable
-            />
-        </Box>
+        <Content
+            categories={categories}
+            clickable
+        />
     )
 }
 export const dynamic = 'force-dynamic';
